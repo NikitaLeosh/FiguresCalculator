d0067f49d6727e0d1e1f0163bb5ae0e310876352
NikitaLeosh <nikita_l90@live.ru>
2023-01-28 11:55:40 +0300

    New figure added - Rectangle. Unit test is also provided.

---
+++ FigureCalculator.Tests/RectangleTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureCalculator.Tests
+{
+	public class RectangleTests
+	{
+		[Fact]
+		public void Rectangle_Area_ReturnsDouble()
+		{
+			//Arrange
+			Rectangle rect = new(2, 3);
+			//Act
+			var area = rect.Area();
+			//Assert
+			area.Should().BeOfType(typeof(double));
+			area.Should().Be(6);
+		}
+	}
+}
---
+++ MindBoxProject/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureCalculator
+{
+	public class Rectangle : Figure
+	{
+		private readonly double _length;
+		private readonly double _width;
+		public Rectangle(double length, double width) : base(0)
+		{
+			_length = length;
+			_width = width;
+		}
+		public override double Area()
+		{
+			return _length * _width;
+		}
+	}
+}
